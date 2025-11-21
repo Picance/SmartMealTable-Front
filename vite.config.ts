023df81bb74de92619590b6d15d6feb6b345d318
seungmin-lee-dotcom <k07217@naver.com>
2025-11-21 22:34:27 +0900

    Fix: PWA 아이콘 파일 경로 업데이트 및 불필요한 아이콘 파일 삭제; HTML 메타 태그 추가

--- vite.config.ts
+++ vite.config.ts
@@ -21,23 +21,23 @@ export default defineConfig({
         orientation: "portrait",
         icons: [
           {
-            src: "pwa-64x64.png",
+            src: "64.png",
             sizes: "64x64",
             type: "image/png",
           },
           {
-            src: "pwa-192x192.png",
+            src: "192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any",
           },
           {
-            src: "maskable-icon-512x512.png",
+            src: "512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "maskable",
